5bc434ea36455e1439bebad16753f5d24641bcaa
matheuspzs <matheusnascimentowave@gmail.com>
2023-06-16 21:33:03 -0300

    Alteração no copyPassword, pois, o Alert estava tirando o foco do document e por isso não estava copiando

--- script.js
+++ script.js
@@ -2,37 +2,43 @@ let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
-let password = document.querySelector ("#password");
+let password = document.querySelector("#password");
 
-let containerPassword = document.querySelector ("#container-password");
+let containerPassword = document.querySelector("#container-password");
 
 let charset = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"
-let novaSenha = ""; 
+let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
     sizePassword.innerHTML = this.value;
 
 }
 
-function generatePassword (){
+function generatePassword() {
     let pass = "";
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
-    pass += charset.charAt(Math.floor(Math.random() * n ));
-    
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+
     }
 
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
     novaSenha = pass;
- 
-}
-
-function copyPassword () {
-    alert("Senha copiada com sucesso!")
-    
-    navigator.clipboard.writeText(novaSenha);
 
 }
+
+function copyPassword() {
+
+    navigator.clipboard.writeText(novaSenha).then(() => {
+        console.log('Copiado com sucesso!')
+        alert("Senha copiada com sucesso!")
+    }).catch((error) => {
+            console.log(error)
+    })
+
+}
+
+document.querySelector('#container-password').addEventListener("click", copyPassword)
 
